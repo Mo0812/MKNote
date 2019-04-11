b7d0c09d02ef52500fda7e7652de3415f0a82cc9
Moritz Kanzler <developer@moritzkanzler.de>
2019-04-11 10:08:33 +0200

    first implementation of note tree

--- src/components/NoteTree/NoteTree.vue
+++ src/components/NoteTree/NoteTree.vue
@@ -1,11 +1,27 @@
 <template>
     <aside class="note-tree">
-        <h2>NoteTree</h2>
+        <b-button @click="test">Add Note</b-button>
+        <ul>
+            <li v-for="note in notes" :key="note.id" @click="remove(note.id)">{{note}}</li>
+        </ul>
     </aside>
 </template>
 
 <script>
 export default {
-    name: "NoteTree"
+    name: "NoteTree",
+    computed: {
+        notes() {
+            return this.$store.getters.getNotes;
+        }
+    },
+    methods: {
+        test() {
+            this.$store.dispatch("addNote", {});
+        },
+        remove(id) {
+            this.$store.dispatch("removeNote", id);
+        }
+    }
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import VuexPersist from "vuex-persist";
+import shortid from "shortid";
 
 import i18n from "./i18n";
 
@@ -10,6 +11,7 @@ const state = {
     note: {
         value: ""
     },
+    notes: [],
     settings: {
         lang: process.env.VUE_APP_I18N_LOCALE,
         size: ""
@@ -18,12 +20,24 @@ const state = {
 // const defaultState = state;
 const getters = {
     getNote: state => state.note,
+    getNotes: state => state.notes,
     getSettings: state => state.settings
 };
 const mutations = {
     NOTE_VALUE: (state, payload) => {
         var note = state.note;
         note.value = payload.value;
+    },
+    NOTE_ADD: (state, payload) => {
+        var notes = state.notes;
+        notes.push(payload);
+    },
+    NOTE_REMOVE: (state, payload) => {
+        var notes = state.notes;
+        notes = notes.filter(element => {
+            return element.id !== payload;
+        });
+        state.notes = notes;
     },
     SETTINGS: (state, payload) => {
         state.settings = payload;
@@ -32,6 +46,17 @@ const mutations = {
 const actions = {
     note_value: (context, payload) => {
         context.commit("NOTE_VALUE", payload);
+    },
+    addNote: (context, payload) => {
+        const newId = shortid.generate();
+        const newNode = {
+            id: newId,
+            value: ""
+        };
+        context.commit("NOTE_ADD", newNode);
+    },
+    removeNote: (context, payload) => {
+        context.commit("NOTE_REMOVE", payload);
     },
     settings: (context, payload) => {
         i18n.locale = payload.lang;
